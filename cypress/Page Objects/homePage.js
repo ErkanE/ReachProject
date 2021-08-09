a684e7719364eec8c5c0dac245b67aa9d22d3101
Erkan Eren <erkan.eren@hotmail.co.uk>
2021-08-09 13:48:39 +0100

    test updates

--- cypress/Page Objects/homePage.js
+++ cypress/Page Objects/homePage.js
@@ -3,8 +3,12 @@
 export default class homePage {
 
     visitPage = () => cy.visit("https://www.medicines.org.uk/emc/browse-companies");
-    
-    clickAcceptCookies = () => cy.get('#onetrust-accept-btn-handler');
+
+    clickCookieSettings = () => cy.get('#onetrust-pc-btn-handler').click();
+
+    clickConfirmCookieSettings = () => cy.get('#accept-recommended-btn-handler').click();
+
+    AcceptCookiesButton = () => cy.get('#onetrust-accept-btn-handler');
 
     getFirstRecord = () => cy.get('.ieleft > ul > li').first();
 
